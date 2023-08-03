42aa7d6f9d5b47c4beb1c2d3d25c683115998048
smit shah <shahsmit293@gmail.com>
2023-08-03 00:44:06 +0000

    Final Chages

--- src/app.js
+++ src/app.js
@@ -11,33 +11,34 @@ window.onload = function() {
     {
       name: "Mrs. Brown",
       image: "https://randomuser.me/api/portraits/women/17.jpg",
-      description: "loves to read about history and philosophy",
+      description: "loves to read about history and philosophy"
     },
     {
       name: "Andrew",
       image: "https://randomuser.me/api/portraits/men/25.jpg",
-      description: "tall, a real state agent",
+      description: "tall, a real state agent"
     },
     {
       name: "Sam",
       image: "https://randomuser.me/api/portraits/men/52.jpg",
-      description: "short, vice president of AT&T sales",
+      description: "short, vice president of AT&T sales"
     },
     {
       name: "Margie",
       image: "https://randomuser.me/api/portraits/women/42.jpg",
-      description: "short, works as a designer for some company",
-    },
+      description: "short, works as a designer for some company"
+    }
   ];
 
-  // INSTRUCTIONS
-  // randomly choose a character and then update using JS:
-  // 1. the card's title with selected character's name
-  // 2. the card's image using selected character's image
-  // 3. the card's body using selected character's description
+  var selectedCharacter =
+    characters[Math.floor(Math.random() * characters.length)];
 
-  // HELP
-  // this is how you set the image src
-  // const image = document.querySelector("#character-image")
-  // image.src = <url to the image>
+  var title = document.querySelector("div.card-body h5.card-title");
+  title.textContent = selectedCharacter.name;
+
+  var descriptionElement = document.querySelector("div.card-body p.card-text");
+  descriptionElement.textContent = selectedCharacter.description;
+
+  var image = document.querySelector("#character-image");
+  image.src = selectedCharacter.image;
 };
